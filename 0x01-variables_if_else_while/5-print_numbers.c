5ac8e2c533740a837bd6a3eb0c3acdaa3701838a
fidele ngendahimana <ngendafidele.104@gmail.com>
2023-03-18 13:31:27 -0700

    print_numberz.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function number
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		printf("%d", a)
+	}
+	printf("\n");
+	return (0);
+}
