3cd8488c98645609905f883f0d524ad9a149d3ba
fidele ngendahimana <ngendafidele.104@gmail.com>
2023-03-18 14:22:39 -0700

    print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main string
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		if (a == 9)
+		{
+			putchar(a + '0');
+		}
+		else
+		{
+			putchar(a + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
+
+
