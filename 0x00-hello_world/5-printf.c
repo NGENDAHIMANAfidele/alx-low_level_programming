eb8ece96de8f4a16a8b75dd0d5d2bd0fbcc1a633
fidele ngendahimana <ngendafidele.104@gmail.com>
2023-03-20 09:01:28 -0700

    print and puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - your program should complie without warning
+ * Return: always 0;
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
