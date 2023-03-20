2f44e660e6c14a962ec33ec19c17f8fa64bec176
fidele ngendahimana <ngendafidele.104@gmail.com>
2023-03-20 13:27:04 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Three number
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		for  (b = 0 ; b < 10 ; b++)
+		{
+			for (c = 0 ; c < 10 ; c++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+					if (a + b + c != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
