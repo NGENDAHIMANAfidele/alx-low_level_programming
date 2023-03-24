2a10c130e486535a0495b566f6ace08f550dc5b0
fidele ngendahimana <ngendafidele.104@gmail.com>
2023-03-24 14:05:10 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - chack paramete 0 and 9
+ * Return: 1 and 0
+ */
+int _isdigit(int c)
+{
+	if (c >=48 && c <= 57)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
