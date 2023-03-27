76e2e2efde59560ee00e7ea2473d764bd8e3b351
fidele ngendahimana <ngendafidele.104@gmail.com>
2023-03-27 11:11:36 -0700

    puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - print half
+ * @str: input
+ * Return: string
+ */
+void puts2(char *str)
+{
+	int a, n, longi;
+
+	longi = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+	long++;
+n = (longi / 2);
+if ((longi % 2) == 1)
+	n = ((longi + 1) / 2);
+for (a = n; str[a] != '\0'; a++)
+	_putchar(str[a]);
+_putcharr('\n');
+}
