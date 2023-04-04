59b05df109de242a14d906c17d7a088ce84913cb
fidele ngendahimana <ngendafidele.104@gmail.com>
2023-04-04 07:11:38 -0700

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,19 +10,19 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int j;
 	int i = 0;
 
-	 while (*s)
-	 {
-		 for (j = 0; accept[j]; j++)
-		 {
-			 if (*s == accept[j])
-			 {
-				 i++;
-				 break;
-			 }
-			 else if (accept[j + 1] == '\0')
-				 return (i);
-		 }
-		 s++;
-	 }
-	 return (i);
+	while (*s)
+	{
+	for (j = 0; accept[j]; j++)
+	{
+	if (*s == accept[j])
+	{
+	i++;
+	break;
+	}
+	else if (accept[j + 1] == '\0')
+	return (i);
+	}
+	s++;
+	}
+	return (i);
 }
