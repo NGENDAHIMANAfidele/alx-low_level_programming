6551deeab0baf990ad9b0fcb8dc31cb91680ba7f
fidele ngendahimana <ngendafidele.104@gmail.com>
2023-04-11 10:07:36 -0700

    create_array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - print the newline
+ * @size: memoly allocation
+ * @c:input
+ * Return: output
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr;
+
+	if (size == 0)
+	{
+	return (NULL);
+	}
+	arr = (char *) malloc(size * sizeof(char));
+	if (arr == NULL)
+	{
+	return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+	arr[i] = c;
+	}
+	return (arr);
+}
