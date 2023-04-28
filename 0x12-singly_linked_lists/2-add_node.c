ff68ac283a035abf090cd8020ab476d167e80180
fidele ngendahimana <ngendafidele.104@gmail.com>
2023-04-28 11:27:54 -0700

    add

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,30 +1,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-typedef struct list_s {
-    char *str;
-    struct list_s *next;
-} list_t;
+#include <stdio.h>
+/**
+ * add_node - print list
+ * @head: first head
+ * @str: string
+ * Retur:sring
+ */
+list_t *add_node(list_t **head, const char *str)	
+{
+	list_t *new;
+	unsigned int len = 0;
 
-list_t *add_node(list_t **head, const char *str) {
-    if (str == NULL) {
-        return NULL;
-    }
-    
-    list_t *new_node = malloc(sizeof(list_t));
-    if (new_node == NULL) {
-        return NULL;
-    }
-    
-    char *str_copy = strdup(str);
-    if (str_copy == NULL) {
-        free(new_node);
-        return NULL;
-    }
-    
-    new_node->str = str_copy;
-    new_node->next = *head;
-    *head = new_node;
-    
-    return new_node;
+	while (str[len])
+		len++;
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
 }
--- 0x12-singly_linked_lists/2-main.c
+++ 0x12-singly_linked_lists/2-main.c
@@ -1,4 +1,4 @@
-a#include <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
