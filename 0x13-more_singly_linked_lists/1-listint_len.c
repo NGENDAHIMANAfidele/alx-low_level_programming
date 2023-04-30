98a1482f7951801a2f20449adc68bcb4788145ce
fidele ngendahimana <ngendafidele.104@gmail.com>
2023-04-30 09:06:08 -0700

    print len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements
+ * @h: A pointer to the head
+ * Return: The number of elements in the list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+	{
+	return (count);
+	}
+
+	i++;
+
+	while (h->next != NULL)
+	{
+	i++;
+	h = h->next;
+	}
+
+	return (i);
+}
