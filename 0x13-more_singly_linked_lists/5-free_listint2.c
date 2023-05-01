767db68e16fdd73b586182db67e9cbb8a6d9818c
fidele ngendahimana <ngendafidele.104@gmail.com>
2023-05-01 07:37:03 -0700

    free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - function frees
+ * @head:pointer
+ * Return: next free
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *next;
+
+	if (head == NULL)
+	return;
+
+	current = *head;
+
+	while (current != NULL)
+	{
+	next = current->next;
+	free(current);
+	current = next;
+	}
+
+	*head = NULL;
+}
