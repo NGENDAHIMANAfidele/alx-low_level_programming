7e977f561cc0532bab5771720611bd1e823ac834
fidele ngendahimana <ngendafidele.104@gmail.com>
2023-05-02 05:36:37 -0700

    reverse

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,30 +1,55 @@
 #include "lists.h"
 #include <stdlib.h>
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+{
+	listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+}
+
 /**
- * free_listint_safe - function for safe
- * @h:pointer
- * Return:return the output
+ * free_listint_safe - Frees a listint_t list.
+ * @h: A pointer to the pointer to the head of the list.
+ *
+ * Return: The number of nodes in the freed list.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count;
-	listint_t *current;
-	listint_t *next;
+    listint_t **list = NULL;
+    listint_t *next;
+    size_t i, num;
 
-	count = 0;
-	current = *h;
-
-	while (current != NULL)
-	{
-	next = current->next;
-	free(current);
-	count++;
-	if (next == NULL || next < current)
-	{
-	break;
-	}
-	current = next;
-	}
-	*h = NULL;
-	return (count);
+    num = 0;
+    if (h == NULL || *h == NULL)
+	    return (num);
+    while (*h != NULL)
+    {
+	    for (i = 0; i < num; i++)
+	    {
+		    if (*h == list[i])
+		    {
+			    *h = NULL;
+			    free(list);
+			    return (num);
+		    }
+	    }
+	    num++;
+	    list = _ra(list, num, *h);
+	    next = (*h)->next;
+	    free(*h);
+	    *h = next;
+    }
+    free(list);
+    return (num);
 }
