21ee1910ccf0bab3b63fe553dd9943c90db3fded
fidele ngendahimana <ngendafidele.104@gmail.com>
2023-05-05 10:51:45 -0700

    flips

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int xor_result;
 	unsigned int count;
 
-	xar_result = n ^ m;
+	xor_result = n ^ m;
 	count = 0;
 	while (xor_result != 0)
 	{
